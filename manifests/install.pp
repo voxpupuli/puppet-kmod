5842d1ee8cf1e985b9b7774afd3a0e455808945d
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-04-05 12:24:30 +0200

    initial import - install kernel module with puppet and augeas

---
+++ manifests/install.pp
@@ -0,0 +1,32 @@
+define kmod::install($ensure=present, $command='/bin/true') {
+
+  case $ensure {
+    present: {
+      exec {"modprobe ${name}":
+        unless => "egrep -q '^${name} ' /proc/modules",
+      }
+      
+      augeas {"install module ${name}":
+        context => '/etc/modprobe.d/modprobe.conf',
+        changes => [
+          "set /install[. = '${name}'] ${name}",
+          "set /install[. = '${name}']/command '${command}'",
+        ],
+      }
+    }
+
+    absent: {
+      exec {"modprobe -r ${name}": 
+        onlyif => "egrep -q '^${name} ' /proc/modules",
+      }
+
+      augeas {"remove module ${name}":
+        context => '/etc/modprobe.d/modprobe.conf',
+        changes => "rm /install[. = '${name}']",
+      }
+    }
+
+    default: { err ( "unknown ensure value ${ensure}" ) }
+  }
+
+}
