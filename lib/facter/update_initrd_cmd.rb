8f1ac84a8ce1a0faa98918c571f3dde25340bf24
Robert Vincent <robert.vincent@conning.com>
2019-04-25 20:25:01 +0000

    Use Factor::Util::Resolution.which()

--- lib/facter/update_initrd_cmd.rb
+++ lib/facter/update_initrd_cmd.rb
@@ -3,40 +3,21 @@
 #
 # Determine which command (if any) must be run to regenerate the initramfs
 # image file for the currently-running kernel.
-require 'facter'
-
 #
-# Function that behaves similarly to the "which" command.
-#
-def which(cmd)
-  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-    exts.each { |ext|
-      exe = File.join(path, "#{cmd}#{ext}")
-      return exe if File.executable?(exe) && !File.directory?(exe)
-    }
-  end
-  return nil
-end
-
-#
-# Use dracut if it is available,
-# or update-initramfs if it exists,
-# or else return nil.
+# Use update-initramfs if it is availble,
+# else use dracut if it is available,
+# else return nil.
 #
 Facter.add(:kmod_update_initrd_cmd) do
-  confine Facter.value(:kernel)=> 'Linux'
   setcode do
-    cmd = which('dracut')
-    if cmd.nil? then
-      cmd = which('update-initramfs')
-      if cmd.nil? then
-        nil
-      else
-        "#{cmd} -u"
+    cmd = Facter::Util::Resolution.which('update-initramfs')
+    if cmd.nil?
+      cmd = Facter::Util::Resolution.which('dracut')
+      unless cmd.nil?
+        "#{cmd} -H -f"
       end
     else
-      "#{cmd} -H f"
+      "#{cmd} -u"
     end
   end
 end
