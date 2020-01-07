8ac0bbe5628d5f50f4ad85eea347daa69c842917
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-01-07 16:30:24 +0100

    Convert to PDK (#54)
    * Convert to PDK
    
    * Rubocop autocorrect
    
    * Fix Style/RegexpLiteral
    
    * Remove spec.opts

--- spec/defines/kmod_load_spec.rb
+++ spec/defines/kmod_load_spec.rb
@@ -1,92 +1,108 @@
 require 'spec_helper'
 
-describe 'kmod::load', :type => :define do
+describe 'kmod::load', type: :define do
   let(:title) { 'foo' }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge( { :augeasversion => '1.2.0' } )
+        facts.merge(augeasversion: '1.2.0')
       end
 
       context 'with ensure set to present' do
-        let(:params) do { :ensure => 'present', :file => '/foo/bar' } end
-        it { should contain_kmod__load('foo') }
-        it { should contain_exec('modprobe foo').with({'unless' => "egrep -q '^foo ' /proc/modules"}) }
+        let(:params) { { ensure: 'present', file: '/foo/bar' } }
+
+        it { is_expected.to contain_kmod__load('foo') }
+        it {
+          is_expected.to contain_exec('modprobe foo')
+            .with('unless' => "egrep -q '^foo ' /proc/modules")
+        }
 
         context 'when on systemd' do
           let(:facts) do
-            facts.merge( { :service_provider => 'systemd' } )
+            facts.merge(service_provider: 'systemd')
           end
 
-          it { should contain_file('/etc/modules-load.d/foo.conf').with({
-            'ensure' => 'present',
-            'mode' => '0644',
-            'content' => "# This file is managed by the puppet kmod module.\nfoo\n"
-          }) }
+          it {
+            is_expected.to contain_file('/etc/modules-load.d/foo.conf')
+              .with('ensure' => 'present',
+                    'mode' => '0644',
+                    'content' => "# This file is managed by the puppet kmod module.\nfoo\n")
+          }
         end
 
         context 'when not on systemd' do
           case facts[:osfamily]
           when 'Debian'
-            it { should contain_augeas('Manage foo in /foo/bar').with({
-              'incl'    => '/foo/bar',
-              'lens'    => 'Modules.lns',
-              'changes' => "clear 'foo'"
-            }) }
+            it {
+              is_expected.to contain_augeas('Manage foo in /foo/bar')
+                .with('incl' => '/foo/bar',
+                      'lens'    => 'Modules.lns',
+                      'changes' => "clear 'foo'")
+            }
           when 'Suse'
-            it { should contain_augeas('sysconfig_kernel_MODULES_LOADED_ON_BOOT_foo').with({
-              'incl'    => '/foo/bar',
-              'lens'    => 'Shellvars_list.lns',
-              'changes' => "set MODULES_LOADED_ON_BOOT/value[.='foo'] 'foo'"
-            }) }
+            it {
+              is_expected.to contain_augeas('sysconfig_kernel_MODULES_LOADED_ON_BOOT_foo')
+                .with('incl' => '/foo/bar',
+                      'lens'    => 'Shellvars_list.lns',
+                      'changes' => "set MODULES_LOADED_ON_BOOT/value[.='foo'] 'foo'")
+            }
           when 'RedHat'
-            it { should contain_file('/etc/sysconfig/modules/foo.modules').with({
-              'ensure'  => 'present',
-              'mode'    => '0755',
-              'content' => /exec \/sbin\/modprobe foo > \/dev\/null 2>&1/
-            })}
+            it {
+              is_expected.to contain_file('/etc/sysconfig/modules/foo.modules')
+                .with('ensure' => 'present',
+                      'mode'    => '0755',
+                      'content' => %r{exec /sbin/modprobe foo > /dev/null 2>&1})
+            }
           end
         end
       end
 
       context 'with ensure set to absent' do
-        let(:params) do { :ensure => 'absent', :file => '/foo/bar' } end
-        it { should contain_kmod__load('foo') }
-        it { should contain_exec('modprobe -r foo').with({ 'onlyif' => "egrep -q '^foo ' /proc/modules" }) }
+        let(:params) { { ensure: 'absent', file: '/foo/bar' } }
+
+        it { is_expected.to contain_kmod__load('foo') }
+        it {
+          is_expected.to contain_exec('modprobe -r foo')
+            .with('onlyif' => "egrep -q '^foo ' /proc/modules")
+        }
 
         context 'when on systemd' do
           let(:facts) do
-            facts.merge( { :service_provider => 'systemd' } )
+            facts.merge(service_provider: 'systemd')
           end
 
-          it { should contain_file('/etc/modules-load.d/foo.conf').with({
-            'ensure' => 'absent',
-            'mode' => '0644',
-            'content' => "# This file is managed by the puppet kmod module.\nfoo\n"
-          }) }
+          it {
+            is_expected.to contain_file('/etc/modules-load.d/foo.conf')
+              .with('ensure' => 'absent',
+                    'mode' => '0644',
+                    'content' => "# This file is managed by the puppet kmod module.\nfoo\n")
+          }
         end
 
         context 'when not on systemd' do
           case facts[:osfamily]
           when 'Debian'
-            it { should contain_augeas('Manage foo in /foo/bar').with({
-              'incl'    => '/foo/bar',
-              'lens'    => 'Modules.lns',
-              'changes' => "rm 'foo'"
-            })}
+            it {
+              is_expected.to contain_augeas('Manage foo in /foo/bar')
+                .with('incl' => '/foo/bar',
+                      'lens'    => 'Modules.lns',
+                      'changes' => "rm 'foo'")
+            }
           when 'Suse'
-            it { should contain_augeas('sysconfig_kernel_MODULES_LOADED_ON_BOOT_foo').with({
-              'incl'    => '/foo/bar',
-              'lens'    => 'Shellvars_list.lns',
-              'changes' => "rm MODULES_LOADED_ON_BOOT/value[.='foo']"
-            }) }
+            it {
+              is_expected.to contain_augeas('sysconfig_kernel_MODULES_LOADED_ON_BOOT_foo')
+                .with('incl' => '/foo/bar',
+                      'lens'    => 'Shellvars_list.lns',
+                      'changes' => "rm MODULES_LOADED_ON_BOOT/value[.='foo']")
+            }
           when 'RedHat'
-            it { should contain_file('/etc/sysconfig/modules/foo.modules').with({
-              'ensure'  => 'absent',
-              'mode'    => '0755',
-              'content' => /exec \/sbin\/modprobe foo > \/dev\/null 2>&1/
-            })}
+            it {
+              is_expected.to contain_file('/etc/sysconfig/modules/foo.modules')
+                .with('ensure' => 'absent',
+                      'mode'    => '0755',
+                      'content' => %r{exec /sbin/modprobe foo > /dev/null 2>&1})
+            }
           end
         end
       end
--- spec/defines/kmod_setting_spec.rb
+++ spec/defines/kmod_setting_spec.rb
@@ -1,40 +1,41 @@
 require 'spec_helper'
 
-describe 'kmod::setting', :type => :define do
+describe 'kmod::setting', type: :define do
   let(:title) { 'foo' }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge({
-          :augeasversion => '1.2.0',
-        })
+        facts.merge(augeasversion: '1.2.0')
       end
 
-      let(:default_params) do { :file => 'modprobe.conf'  } end
-      let(:params) do default_params end
+      let(:default_params) { { file: 'modprobe.conf' } }
+      let(:params) { default_params }
 
       context 'add an alias' do
-        let(:params) do default_params.merge({ :category => 'alias', :option => 'modulename', :value => 'tango' }) end
-        it { should contain_kmod__setting('foo')}
-        it { should contain_augeas('kmod::setting foo foo').with({
-          'incl'    => 'modprobe.conf',
-          'lens'    => 'Modprobe.lns',
-          'changes' => [ "set alias[. = 'foo'] foo", "set alias[. = 'foo']/modulename tango" ],
-          'require' => 'File[modprobe.conf]'
-        })}
+        let(:params) { default_params.merge(category: 'alias', option: 'modulename', value: 'tango') }
+
+        it { is_expected.to contain_kmod__setting('foo') }
+        it {
+          is_expected.to contain_augeas('kmod::setting foo foo')
+            .with('incl'    => 'modprobe.conf',
+                  'lens'    => 'Modprobe.lns',
+                  'changes' => ["set alias[. = 'foo'] foo", "set alias[. = 'foo']/modulename tango"],
+                  'require' => 'File[modprobe.conf]')
+        }
       end
       context 'add a blacklist' do
-        let(:params) do { :file => '/etc/modprobe.d/blacklist.conf', :category => 'blacklist'  } end
-        it { should contain_kmod__setting('foo')}
-        it { should contain_augeas('kmod::setting foo foo').with({
-          'incl'    => '/etc/modprobe.d/blacklist.conf',
-          'lens'    => 'Modprobe.lns',
-          'changes' => [ "set blacklist[. = 'foo'] foo" ],
-          'require' => 'File[/etc/modprobe.d/blacklist.conf]'
-        })}
-      end
+        let(:params) { { file: '/etc/modprobe.d/blacklist.conf', category: 'blacklist' } }
 
+        it { is_expected.to contain_kmod__setting('foo') }
+        it {
+          is_expected.to contain_augeas('kmod::setting foo foo')
+            .with('incl'    => '/etc/modprobe.d/blacklist.conf',
+                  'lens'    => 'Modprobe.lns',
+                  'changes' => ["set blacklist[. = 'foo'] foo"],
+                  'require' => 'File[/etc/modprobe.d/blacklist.conf]')
+        }
+      end
     end
   end
 end
