3f81521460552adbfcb6578c46713a84a7b8efd4
Tim Meusel <tim@bastelfreak.de>
2022-02-02 12:37:54 +0100

    rubocop: Fix Style/FrozenStringLiteralComment

--- lib/facter/kmod.rb
+++ lib/facter/kmod.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Facter.add(:kmods) do
   confine kernel: :linux
 
--- spec/defines/kmod_load_spec.rb
+++ spec/defines/kmod_load_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'kmod::load', type: :define do
