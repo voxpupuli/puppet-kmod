e459ed7c1b71b0f774a21ca7140cc6e4f5375778
Pat Riehecky <riehecky@fnal.gov>
2022-02-03 11:16:26 -0600

    Hide read error on params when you can't read them
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>

--- lib/facter/kmod.rb
+++ lib/facter/kmod.rb
@@ -20,8 +20,12 @@ Facter.add(:kmods) do
             Dir.foreach("/sys/module/#{directory}/parameters") do |param|
               next if ['.', '..'].include?(param)
 
+              next unless File.readable?("/sys/module/#{directory}/parameters/#{param}")
+
               kmod[directory]['parameters'][param] = File.read("/sys/module/#{directory}/parameters/#{param}").chomp
             end
+          rescue Errno::EACCES => e
+            Facter.debug(e)
           rescue StandardError => e
             Facter.warn(e)
           end
